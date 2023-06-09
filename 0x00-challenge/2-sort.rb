ba9f65ae4ac07e17cda3d1f37675fa410ce3f065
mwangiii <mwangi8kevin@gmail.com>
2023-06-09 23:49:04 +0300

    ruby debugging

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -5,27 +5,28 @@
 ###
 
 result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+ARGV.each do |arg|
+  next if arg !~ /^-?[0-9]+$/
+
+  i_arg = arg.to_i
+
+  is_inserted = false
+  i = 0
+  l = result.size
+
+  while !is_inserted && i < l do
+    if result[i] < i_arg
+      i += 1
+    else
+      result.insert(i, i_arg)  # Corrected line
+      is_inserted = true
+      break
     end
-    result << i_arg if !is_inserted
+  end
+
+  result << i_arg if !is_inserted
 end
 
 puts result
+
